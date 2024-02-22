094225221d3b9c5c5b7b90828a308afdf19ff67e
oriohac <oriohac@gmail.com>
2024-02-22 13:30:01 +0100

    completed the http methods code

--- lib/other_methods.dart
+++ lib/other_methods.dart
@@ -15,9 +15,11 @@ class Datamodel {}
 class _OtherMethodsState extends State<OtherMethods> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    
+   
+
+    return SafeArea(
+      child: Scaffold(
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(12.0),
@@ -34,7 +36,9 @@ class _OtherMethodsState extends State<OtherMethods> {
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(6))),
                         onPressed: () {
-                          postData();
+                          setState(() {
+                            postData();
+                          });
                         },
                         child: const Text(
                           "POST",
@@ -52,7 +56,9 @@ class _OtherMethodsState extends State<OtherMethods> {
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(6))),
                         onPressed: () {
-                          deleteData();
+                          setState(() {
+                            deleteData();
+                          });
                         },
                         child: const Text(
                           "DELETE",
@@ -70,7 +76,9 @@ class _OtherMethodsState extends State<OtherMethods> {
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(6))),
                         onPressed: () {
-                          putData();
+                          setState(() {
+                            putData();
+                          });
                         },
                         child: const Text(
                           "PUT",
@@ -86,121 +94,128 @@ class _OtherMethodsState extends State<OtherMethods> {
     );
   }
 
+Future<void> postData() async {
+      var response = await http.post(
+          Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode({
+            "userId": '2',
+            "title": 'Introduction to API',
+            "body": 'This is just a demo of what you can do'
+          }));
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Color.fromARGB(255, 122, 245, 126),
+          duration: Duration(seconds: 7),
+          content: Text("Post Created Successfuly"),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(36))),
+          showCloseIcon: true,
+          dismissDirection: DismissDirection.horizontal,
+          closeIconColor: Colors.white,
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Row(
+            children: [
+              Icon(
+                Icons.warning_amber,
+                color: Colors.yellow,
+              ),
+              SizedBox(
+                width: 18,
+              ),
+              Text("Something went wrong!")
+            ],
+          ),
+          backgroundColor: Colors.red,
+        ));
+        throw Exception("Could not Post Data");
+      }
+    }
+
+
+
   Future<void> deleteData() async {
-    var response = await http
-        .delete(Uri.parse('https://jsonplaceholder.typicode.com/posts/${3}'));
-    if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: Colors.green,
-        duration: Duration(seconds: 7),
-        content: Text("Post delete Success"),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(36))),
-        showCloseIcon: true,
-        dismissDirection: DismissDirection.horizontal,
-        closeIconColor: Colors.white,
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        content: Row(
-          children: [
-            Icon(
-              Icons.warning_amber,
-              color: Colors.yellow,
-            ),
-            SizedBox(
-              width: 18,
-            ),
-            Text("Something went wrong!")
-          ],
-        ),
-        backgroundColor: Colors.red,
-      ));
-      throw Exception("Could not delete Data");
+      var response = await http
+          .delete(Uri.parse('https://jsonplaceholder.typicode.com/posts/${3}'));
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.green,
+          duration: Duration(seconds: 7),
+          content: Text("Post delete Success"),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(36))),
+          showCloseIcon: true,
+          dismissDirection: DismissDirection.horizontal,
+          closeIconColor: Colors.white,
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Row(
+            children: [
+              Icon(
+                Icons.warning_amber,
+                color: Colors.yellow,
+              ),
+              SizedBox(
+                width: 18,
+              ),
+              Text("Something went wrong!")
+            ],
+          ),
+          backgroundColor: Colors.red,
+        ));
+        throw Exception("Could not delete Data");
+      }
     }
-  }
-  Future<void> postData() async{
-    var response = await http.post(Uri.parse('https://jsonplaceholder.typicode.com/posts/2'),
-    headers: {"content-type":"application/json"},
-    body: jsonEncode({
-      "userId": '2',
-      "title": 'Introduction to API',
-      "body": 'This is just a demo of what you can do'
-  })
-    );
-    if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: Color.fromARGB(255, 122, 245, 126),
-        duration: Duration(seconds: 7),
-        content: Text("Post Created Successfuly"),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(36))),
-        showCloseIcon: true,
-        dismissDirection: DismissDirection.horizontal,
-        closeIconColor: Colors.white,
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        content: Row(
-          children: [
-            Icon(
-              Icons.warning_amber,
-              color: Colors.yellow,
-            ),
-            SizedBox(
-              width: 18,
-            ),
-            Text("Something went wrong!")
-          ],
-        ),
-        backgroundColor: Colors.red,
-      ));
-      throw Exception("Could not Post Data");
+
+    
+    Future<void> putData() async {
+      var response = await http.put(
+          Uri.parse('https://jsonplaceholder.typicode.com/posts/2'),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode({
+            "userId": '2',
+            "title": 'Introduction to API',
+            "body":
+                'You have learnt some of these concepts now is the time to apply them.'
+          }));
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Color.fromARGB(255, 122, 245, 126),
+          duration: Duration(seconds: 7),
+          content: Text("Post Updated Successfuly"),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(36))),
+          showCloseIcon: true,
+          dismissDirection: DismissDirection.horizontal,
+          closeIconColor: Colors.white,
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Row(
+            children: [
+              Icon(
+                Icons.warning_amber,
+                color: Colors.yellow,
+              ),
+              SizedBox(
+                width: 18,
+              ),
+              Text("Something went wrong!")
+            ],
+          ),
+          backgroundColor: Colors.red,
+        ));
+        throw Exception("Could not update Data");
+      }
     }
-  }
-  Future<void> putData() async{
-    var response = await http.put(Uri.parse('https://jsonplaceholder.typicode.com/posts/2'),
-    headers: {"content-type":"application/json"},
-    body: jsonEncode({
-      "userId": '2',
-      "title": 'Introduction to API',
-      "body": 'You have learnt some of these concepts now is the time to apply them.'
-  })
-    );
-    if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: Color.fromARGB(255, 122, 245, 126),
-        duration: Duration(seconds: 7),
-        content: Text("Post Updated Successfuly"),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(36))),
-        showCloseIcon: true,
-        dismissDirection: DismissDirection.horizontal,
-        closeIconColor: Colors.white,
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        behavior: SnackBarBehavior.floating,
-        content: Row(
-          children: [
-            Icon(
-              Icons.warning_amber,
-              color: Colors.yellow,
-            ),
-            SizedBox(
-              width: 18,
-            ),
-            Text("Something went wrong!")
-          ],
-        ),
-        backgroundColor: Colors.red,
-      ));
-      throw Exception("Could not update Data");
-    }
-  }
+
 }
